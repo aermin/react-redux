574fe1b71d9c473894fea4523a96e515684aa8dd
aermin <aermin@foxmail.com>
2018-07-22 22:52:51 +0800

    结合context和store（react-redux1.0）

---
+++ src/ThemeSwitch.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ThemeSwitch extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  constructor () {
+    super()
+    this.state = { themeColor: '' }
+  }
+
+  _updateThemeColor () {
+    const { store } = this.context
+    const state = store.getState()
+    this.setState({ themeColor: state.themeColor })
+  }
+
+  componentWillMount () {
+    const { store } = this.context;
+    this._updateThemeColor();
+    store.subscribe(() => this._updateThemeColor());
+  }
+
+  changeColor(color){
+     const {store}= this.context;
+     store.dispatch({
+       type:'CHANGE_COLOR',
+       themeColor: color
+     })
+  }
+
+  render () {
+    return (
+      <div>
+        <button style={{ color: this.state.themeColor }}
+        onClick={this.changeColor.bind(this,'red')}>Red</button>
+        <button style={{ color: this.state.themeColor }}
+        onClick={this.changeColor.bind(this,'blue')}>Blue</button>
+      </div>
+    )
+  }
+}
+
+export default ThemeSwitch
